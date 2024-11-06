6a64d193a7ccb8d7aacde23d9d9bffc8476be801
puppyChen(Jake) <puppychen@gmail.com>
2024-11-06 12:17:08 +0800

    fix: cors

--- index.ts
+++ index.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
+import * as cors from 'cors';
 import * as express from 'express';
 import * as functions from 'firebase-functions/v2';
 import { AppModule } from './src/app.module';
 
 const expressServer = express();
-
+expressServer.use(cors());
 const createFunction = async (expressInstance): Promise<void> => {
   const app = await NestFactory.create(
     AppModule,
